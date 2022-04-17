8c397353f798d2a7706a364e31da8cbe3f830255
ZubayerAhammedTolon <zs.tolon0073@gmail.com>
2022-04-17 07:34:35 +0600

    deploy steps wrote

--- src/__auth_steps.js
+++ src/__auth_steps.js
@@ -21,8 +21,9 @@
 
 /* 
 * hosting steps
-1. npm install -g firebase-tools 
-2. firebase login
-3. firebase init
-4. firebase deploy
+1. npm install -g firebase-tools (one time)
+2. firebase login (one time)
+3. firebase init(one time for each project)
+4. npm run build(build your project) (every time)
+5. firebase deploy(every time)
 */
